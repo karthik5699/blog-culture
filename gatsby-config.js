0682ea2a1bee9a2eba3f8c7dc117a621e960b019
Katic <karthiksingh5699@gmail.com>
2023-08-17 12:01:17 +0530

    page query gatsby data layer for blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,18 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `first_blog`,
+    title: `Blog - Culture`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+  ],
 }
